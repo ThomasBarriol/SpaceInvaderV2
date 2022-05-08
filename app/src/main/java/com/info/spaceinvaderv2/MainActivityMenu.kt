fa8b3d7a567779ea9c608970a1d28066a8c80aa3
thomasbarriol <thomas.barriol@ulb.be>
2022-05-08 16:34:33 +0200

    Beta Invader V3.2
    - Ajout d'un boutton crédits qui ouvre une boite de dialogue montrant les crédits

--- app/src/main/java/com/info/spaceinvaderv2/MainActivityMenu.kt
+++ app/src/main/java/com/info/spaceinvaderv2/MainActivityMenu.kt
@@ -1,22 +1,30 @@
 package com.info.spaceinvaderv2
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import kotlin.system.exitProcess
 
 class MainActivityMenu : AppCompatActivity(), View.OnClickListener {
     lateinit var buttonPlay: Button
     lateinit var buttonRegle : Button
+    lateinit var buttonCredits : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
         buttonPlay = findViewById(R.id.button_play)
         buttonRegle = findViewById(R.id.regle_button)
+        buttonCredits = findViewById(R.id.button_credit)
         buttonPlay.setOnClickListener(this)
         buttonRegle.setOnClickListener(this)
+        buttonCredits.setOnClickListener(this)
 
     }
 
@@ -24,6 +32,7 @@ class MainActivityMenu : AppCompatActivity(), View.OnClickListener {
         when (v?.id){
             R.id.button_play -> openMainActivity()
             R.id.regle_button -> openRegleActivity()
+            R.id.button_credit -> openCredit()
         }
     }
 
@@ -36,6 +45,32 @@ class MainActivityMenu : AppCompatActivity(), View.OnClickListener {
         val intent = Intent(this, ActivityRules::class.java).apply{}
         startActivity(intent)
 
+    }
+
+    private fun openCredit(){
+        class Credits: DialogFragment() {
+            override fun onCreateDialog(bundle: Bundle?): Dialog {
+                val builder = AlertDialog.Builder(getActivity())
+                builder.setTitle(R.string.credits)
+                builder.setMessage(R.string.developpeurs)
+                builder.setPositiveButton("Retour") { _, _->}
+                return builder.create()
+            }
+        }
+        runOnUiThread(
+            Runnable {
+                val ft = supportFragmentManager.beginTransaction()
+                val prev = supportFragmentManager.findFragmentByTag("dialog")
+                if (prev != null) {
+                    ft.remove(prev)
+                }
+                ft.addToBackStack(null)
+                val credits = Credits()
+                credits.setCancelable(false)
+                credits.show(ft,"dialog")
+            }
+
+        )
     }
 
 
